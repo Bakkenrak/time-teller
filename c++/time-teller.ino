b9b8d1e5341a600f6aeecf57e919cad7415e9044
Christian <crest@DESKTOP-42LNID3>
2024-03-13 21:44:25 +0100

    update arduino variant of time-teller

--- c++/time-teller.ino
+++ c++/time-teller.ino
@@ -1,23 +1,123 @@
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <cmath>
 
-const char *ssid     = "MY_WIFI_SSID";
-const char *password = "changeme";
-
-const long utcOffsetInSeconds = 3600;
-
-// Define NTP Client to get time
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
-
+#define DEBUG
+#define DFPLAYER_DEBUG
+#define DFPLAYER_DEBUG_HEAVY
 
 struct RoundedTime {
     int hour;
     int min;
     int sec;
 };
+
+// WIFI credentials
+const char *ssid     = "MY_WIFI_SID";
+const char *password = "my-wifi-pw";
+
+// Init NTP Client 
+const long utcOffsetInSeconds = 3600;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+// init audio player
+SoftwareSerial mySoftwareSerial(13, 15); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+const int audioPlayerBusyPin = 12;
+
+const byte sensorTriggerPin = 14;
+const byte sensorEchoPin = 16;
+
+void setup()
+{
+  mySoftwareSerial.begin(9600);
+  Serial.begin(115200);
+
+  connectToWifi();
+
+  timeClient.begin();
+  
+  connectAudioPlayer();
+  
+
+  pinMode(sensorTriggerPin, OUTPUT);
+  pinMode(sensorEchoPin, INPUT);
+}
+
+void connectToWifi() {
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+  Serial.println();
+  Serial.println(F("WIFI connected"));
+}
+
+void connectAudioPlayer() {
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(mySoftwareSerial, true, true)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  myDFPlayer.volume(5);  //Set volume value. From 0 to 30
+
+  // configure audio player's busy pin as input
+  pinMode(audioPlayerBusyPin, INPUT);
+}
+
+void loop()
+{
+  delay(1000);
+
+  if (getSensorDistance() < 30) {
+    timeClient.update();
+    tellTime();
+  }
+}
+
+int getSensorDistance() {
+  digitalWrite(sensorTriggerPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(sensorTriggerPin, HIGH);
+  delayMicroseconds(10);
+
+  long duration = pulseIn(sensorEchoPin, HIGH);
+  int distance = duration/58;
+
+  if (distance > 300 || distance <= 0){
+    Serial.println("Out of sensor range!");
+  } else {
+    Serial.println("Distance to object: " + String(distance) + " cm");
+  }
+  return distance;
+}
+
+void waitForReady() {
+  delay(1000); // initial wait to not skip over loading the currently playing file
+  while(!isReadyToPlay()) {
+    delay(50);
+  }
+}
+
+bool isReadyToPlay() {
+  return digitalRead(audioPlayerBusyPin) == HIGH;
+}
 
 RoundedTime getRoundedTime(int hour, int min, int sec) {
     // Calculate total seconds of the hour
@@ -91,38 +191,20 @@ void tellTime() {
   Serial.println(timeClient.getSeconds());
   Serial.print("Spelled out as ");
   Serial.print(hours);
-  Serial.print(":");
+  Serial.print(" - ");
   Serial.println(minutes);
   Serial.println();
 
-  // play "es_ist" (file no 1)
+
+  myDFPlayer.playFolder(1,1);
+  waitForReady();
   if (minutes > 0) {
-    // play minutes (file no 200 + minutes)
+    myDFPlayer.playFolder(3,minutes);
+    waitForReady();
   }
-  // play hours (file no 100 + hours)
+  myDFPlayer.playFolder(2,hours);
+  waitForReady();
   if (minutes == 0) {
-    // play minutes (file no 200)
+    myDFPlayer.playFolder(3,minutes);
   }
-}
-
-void setup(){
-  Serial.begin(115200);
-
-  WiFi.begin(ssid, password);
-
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );
-  }
-
-  timeClient.begin();
-}
-
-void loop() {
-  timeClient.update();
-
-  // if trigger signal:
-  tellTime();
-
-  delay(10000);
 }
